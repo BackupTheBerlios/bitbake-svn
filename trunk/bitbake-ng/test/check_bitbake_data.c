083e51b2f551b7569448c882e8d39daaffd5bf39
kergoth <kergoth@fd90740e-a5ea-0310-a7a2-b3005cc77bd6>
2005-01-09 00:28:14 +0000

    Add actual datastore tests .c file.

---
+++ trunk/bitbake-ng/test/check_bitbake_data.c
@@ -0,0 +1,69 @@
+/* ex:ts=4:sw=4:sts=4:et
+ * -*- tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+
+/** @file metadata_1.c
+ *  @brief First group of metadata tests
+ *
+ * Copyright (C) 2004, 2005 Chris Larson <kergoth@handhelds.org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include <stdlib.h>
+#include <glib.h>
+#include <check.h>
+#include <bitbake.h>
+
+/**
+ * Test the creation and destruction of a struct bb_data
+ * using the bb_data_new() and bb_data_destroy() functions.
+ */
+
+START_TEST (test_data_create_destroy)
+{
+    gpointer data;
+
+    data = bb_data_new();
+    if (data == NULL)
+        fail("Metadata store allocation returned a NULL pointer");
+    bb_data_destroy(data);
+}
+END_TEST
+
+Suite *bitbake_data_suite(void)
+{
+  Suite *s = suite_create("Bitbake Data");
+  TCase *tc_core = tcase_create("Core");
+
+  suite_add_tcase (s, tc_core);
+  tcase_add_test(tc_core, test_data_create_destroy);
+
+  return s;
+}
+
+int main(void)
+{
+  int nf;
+  Suite *s = bitbake_data_suite();
+  SRunner *sr = srunner_create(s);
+  srunner_run_all(sr, CK_NORMAL);
+  nf = srunner_ntests_failed(sr);
+  srunner_free(sr);
+  return (nf == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
