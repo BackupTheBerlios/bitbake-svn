5f600817126b2665eae91af705fd29d2a379a430
zecke123 <zecke123@fd90740e-a5ea-0310-a7a2-b3005cc77bd6>
2005-10-21 21:48:41 +0000

    BitBake QA:     -Add the Option to use different Report Outputs. Currently      only Text exists.     -Add testore_array which will store the results of a test run      skeleton

--- trunk/bitbake_qa/lib/bittest/__init__.py
+++ trunk/bitbake_qa/lib/bittest/__init__.py
@@ -1,11 +1,13 @@
 from testresult import TestItem, TestResult
 from testreport_text import TestReportText
+from teststore_array import TestStoreArray
 from config import parse_test_options
 
 __all__ = [
-	"TestResult",
-	"TestItem",
-	"TestReportText",
+    "TestResult",
+    "TestItem",
+    "TestReportText",
+    "TestStoreArray",
 
 # fileparser.py
 	"fileparser",
