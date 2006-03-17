3f9182d7d0d8cdaf51c556815413a159ed5d9574
zecke123 <zecke123@fd90740e-a5ea-0310-a7a2-b3005cc77bd6>
2006-03-17 23:04:34 +0000

    bitbake-tests:     Add a test to test the bb.mkdirhier method

--- trunk/bitbake-tests/tests/bblib/bblibtest.py
+++ trunk/bitbake-tests/tests/bblib/bblibtest.py
@@ -8,13 +8,60 @@ USE this code for whatever purpose you want to use it
 
 import unittest
 
+_test_dir = 'output'
+
 class BBModuleTestCase(unittest.TestCase):
     """
     Test Case for BB Library Module
     """
 
+    def __init__(self,methodName='runTest'):
+        """
+        Clean the test directory
+        """
+        import os
+        os.system('rm -r %s > /dev/null 2>&1' % os.path.join(_test_dir,'mkdir'))
+        unittest.TestCase.__init__(self,methodName)
+
     def testMkdir(self):
-        pass
+        """
+        Create directories using bb.mkdirhier
+
+        We do that with relative and absolute pathnames
+        """
+        import bb
+        import os
+        local_path = os.getcwd()
+
+        # test before we begin. We do not want any files
+        self.assertFalse(os.path.exists(os.path.join(_test_dir,'mkdir')))
+
+        # create the directories
+        bb.mkdirhier(os.path.join(_test_dir, 'mkdir','foo', 'moo', 'zoo'))
+        self.assertEquals(local_path, os.getcwd(), "Still the same directory1")
+        self.assertTrue(os.path.exists(os.path.join(_test_dir, 'mkdir','foo','moo','zoo')))
+        self.assertFalse(os.path.exists(os.path.join('mkdir')))
+
+        # create now a directory which already exists
+        self.assertTrue(os.path.exists(os.path.join(_test_dir, 'mkdir', 'foo')))
+        bb.mkdirhier(os.path.join(_test_dir, 'mkdir','foo'))
+        self.assertTrue(os.path.exists(os.path.join(_test_dir, 'mkdir', 'foo')))
+        self.assertTrue(os.path.exists(os.path.join(_test_dir, 'mkdir', 'foo', 'moo', 'zoo' )))
+
+
+        # now use absolute paths
+        abs = os.path.join(local_path,_test_dir)
+        self.assertTrue(os.path.exists(abs))
+        bb.mkdirhier(os.path.join(abs,'mkdir','foo','doo'))
+        self.assertTrue(os.path.exists(os.path.join(abs,'mkdir','foo','doo')))
+
+        # check that all created directories are present
+        self.assertTrue(os.path.exists(os.path.join(_test_dir,'mkdir','foo','doo')))
+        self.assertTrue(os.path.exists(os.path.join(_test_dir,'mkdir','foo','moo')))
+        self.assertTrue(os.path.exists(os.path.join(_test_dir,'mkdir','foo','moo','zoo')))
+
+        # check that we have no other side effect
+        self.assertEquals(local_path, os.getcwd())
 
     def testMovefile(self):
         pass
@@ -63,3 +110,7 @@ class BBModuleTestCase(unittest.TestCase):
 
     def testDepOpConvert(self):
         pass
+
+
+if __name__ == '__main__':
+    unittest.main()
