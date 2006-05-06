53db8845bb0ab5e9895dfe57b906e15f7dc6ca87
zecke123 <zecke123@fd90740e-a5ea-0310-a7a2-b3005cc77bd6>
2006-05-06 22:48:09 +0000

    bitbake-tests/tests/bugs:     Add the demonstration from #68 as a regression     test.

--- trunk/bitbake-tests/tests/bugs/regtests.py
+++ trunk/bitbake-tests/tests/bugs/regtests.py
@@ -8,9 +8,10 @@ USE this code for whatever purpose you want to use it
 
 import unittest, os
 
-(path, name) = os.path.split(__file__)
-path = os.path.join(os.path.abspath(path), 'bug_937')
-
+(base_path, name) = os.path.split(__file__)
+base_path = os.path.abspath(base_path)
+path_937 = os.path.join(base_path, 'bug_937')
+path_68  = os.path.join(base_path, 'bug_68')
 
 class RegressionTests(unittest.TestCase):
     """
@@ -44,10 +45,10 @@ class RegressionTests(unittest.TestCase):
                 }
 
         # set BBPATH
-        os.environ['BBPATH'] = os.path.join(path)
+        os.environ['BBPATH'] = os.path.join(path_937)
         bitbake = imp.load_source( "bitbake", os.environ['BITBAKE'] )
 
-        os.chdir(path)
+        os.chdir(path_937)
         cooker = bitbake.BBCooker()
         try:
             cooker.cook( bitbake.BBConfiguration( Opt() ), ["fix-mind"] )
@@ -56,6 +57,50 @@ class RegressionTests(unittest.TestCase):
 
         self.assertEquals("soft", os.environ['BB_TARGET_FPU'])
         self.assertEquals("true", os.environ['BB_BASE_BBCLAS_RAN'])
+
+
+    def testBug68(self):
+        """
+        Selecting the best provider among BitBake collections
+        """
+        import imp, bb
+
+        class Opt:
+            def __init__(self):
+                self.__dict__ = {
+                    'abort' : False,
+                    'interactive' : False,
+                    'force' : False,
+                    'cmd' : 'build',
+                    'file' : [],
+                    'verbose' : False,
+                    'debug' : 0,
+                    'dry_run' : False,
+                    'parse_only' : False,
+                    'disable_psyco' : False,
+                    'show_versions' : True,
+                    'show_environment' : False,
+                    'buildfile' : None
+                }
+
+        # set BBPATH
+        os.environ['BBPATH'] = os.path.join(path_68)
+        bitbake = imp.load_source( "bitbake", os.environ['BITBAKE'] )
+
+        os.chdir(path_68)
+        cooker = bitbake.BBCooker()
+        try:
+            cooker.cook( bitbake.BBConfiguration( Opt() ), [] )
+        except SystemExit:
+            print "exited"
+
+        (last_ver,last_file,pref_ver,pref_file) = cooker.findBestProvider('test')
+
+
+        self.assertEquals( ('100', 'r0'), last_ver)
+        self.assertEquals('collection2/test_100.bb', last_file)
+        self.assertEquals(('100', 'r0'), pref_ver)
+        self.assertEquals('collection2/test_100.bb', pref_file)
 
 
 
