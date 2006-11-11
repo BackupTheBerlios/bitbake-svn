08063d5cab0c447c56dc6ce5812293e3ee2ecd58
zecke123 <zecke123@fd90740e-a5ea-0310-a7a2-b3005cc77bd6>
2006-11-11 22:46:33 +0000

    Parse everything, add it to the list of ASTs and then sleep to analyze memory usage

--- trunk/bitbake-parser/parse-all.py
+++ trunk/bitbake-parser/parse-all.py
@@ -1,6 +1,8 @@
 import bitbakec
 import os
 
+asts = []
+
 for root, dirs, files in os.walk('/space/hacking/embedded/oe/org.openembedded.dev/'):
     for file in files:
         (r2, ext) = os.path.splitext(file)
@@ -9,6 +11,8 @@ for root, dirs, files in os.walk('/space/hacking/embedded/oe/org.openembedded.de
         
         path = os.path.join(root, file)
         #print "Parsing %s" % path
-        bitbakec.parsefile(path,False)
+        asts.append( bitbakec.parsefile(path,False) )
 
 
+import time
+time.sleep( 60000 )
